b640b3be87da10b6b9a070130a0c385a34ba3b2e
Крылов Владислав <vladcrylov@yandex.ru>
2024-10-19 21:58:57 +0300

    Added a geometry program

---
+++ Right-angled triangle/Right-angled triangle.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if ((a + b > c) && (b + c > a) && (a + c > b))
+    {
+        if ((pow(a, 2) == pow(b, 2) + pow(c, 2)) \
+            || (pow(b, 2) == pow(a, 2) + pow(c, 2)) \
+            || (pow(c, 2) == pow(b, 2) + pow(a, 2)))
+            cout << "YES";
+        else cout << "NO";
+    }
+    else {
+        cout << "UNDEFINED";
+    }
+    return 0;
+}
